0bd776e8fc240c6649e96fab6782121c8ee0d22e
Daniel (Ray) <thebyt3zcode@yahoo.com>
2026-03-19 16:25:44 -0300

    flatpak update

--- lumin/install/dnf.sh
+++ lumin/install/dnf.sh
@@ -5,3 +5,5 @@
 sudo dnf update && sudo dnf upgrade
 sudo dnf install flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+flatpak update
--- lumin/install/pac.sh
+++ lumin/install/pac.sh
@@ -3,3 +3,5 @@
 sudo pacman -Syu
 sudo pacman -S flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+flatpak update
