86fc54abddd8e0ac3b54263aec4a7044062b83b8
Nix <thebyt3zcode@yahoo.com>
2026-05-20 23:23:02 -0300

    update pixel/linux

--- lumin/install/pac.sh
+++
@@ -1,9 +0,0 @@
-# Code by Nyan.T64 -w-
-
-sudo pacman -Syu
-sudo pacman -S flatpak git nano
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-
-flatpak update
-
-curl -s https://nyannix.is-a.dev/lumin/install/InstallAny.sh | bash
